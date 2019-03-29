3006d4c3b91d2f58411f224d2714e3acb26658c6
mving <manuelvidal097@gmail.com>
2019-03-29 00:51:22 -0300

    init

---
+++ 28-03/ejercicio_guia.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+int main(void) {
+  int segundos;
+
+  char cadena[MAX];
+  fgets(cadena, MAX, stdin);
+  segundos = atoi(cadena);
+  int s = segundos % 60;
+  segundos /= 60;
+  int m = segundos % 60;
+  int h = segundos / 60;
+  printf("%dh %d' %d\"\n",h,m,s);
+  return 0;
+}
