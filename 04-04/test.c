8a9c56f5e370b4194d38cff39b9204151d943633
mving <manuelvidal097@gmail.com>
2019-04-07 22:22:21 -0300

    nb

--- 04-04/test.c
+++ 04-04/test.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#define NOTA_MAX 10
+//#define NOTA_MAX 10
 
 int main() {
-  int nota = 10;
+/*  int nota = 10;
   int a;
   char b;
   float c;
   int numero =15;
   if (numero >= nota) {
 //    printf("hola\n");
-  }
+}*/
 
 //  printf("a%d\nb%c\nc%f",a,b,c);
-long j = 2147483647;
-printf("%d\n",j);
+//long long j = 214748367000000007878;
+//  long long j = 9999999999999999999;
+  unsigned char j = 255;
+  printf("%d\n",j);
   return 0;
 }
